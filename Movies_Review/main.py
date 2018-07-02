e3ed90d668a6de3d24611871bd34bdef50c07d1a
sherwaldeepesh <deepeshsherwal@gmail.com>
2018-07-02 12:15:27 +0530

    movie review project updated

--- Movies_Review/main.py
+++ Movies_Review/main.py
@@ -22,10 +22,10 @@ genres_unique = pd.DataFrame(genres_unique, columns=['genres']) # Format into Da
 dataset = dataset.join(dataset.genres.str.get_dummies().astype(bool))
 dataset.drop('genres', inplace=True, axis=1)
 
-dataset2 = dataset.drop(['imdb_score','movie_title','movie_imdb_link','language','country','movie_facebook_likes'],axis = 1)
+dataset2 = dataset.drop(['imdb_score','movie_title','movie_imdb_link','language','movie_facebook_likes'],axis = 1)
 
 features = dataset2.iloc[:,0:].values
-labels = dataset.iloc[:,16].values
+labels = dataset.iloc[:,17].values
 
 from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
@@ -33,9 +33,19 @@ labelencoder = LabelEncoder()
 features[:,3] = labelencoder.fit_transform(features[:,3])
 features[:,5] = labelencoder.fit_transform(features[:,5])
 features[:,8] = labelencoder.fit_transform(features[:,8])
+features[:,11] = labelencoder.fit_transform(features[:,11])
 
 for i in range(13,37):
     features[:,i] = labelencoder.fit_transform(features[:,i])
+
+
+
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 3,random_state = 0)
+features = pca.fit_transform(features)
+
     
 from sklearn.model_selection import train_test_split
 features_train,features_test,labels_train,labels_test = train_test_split(features,labels,test_size = 0.2, random_state = 0)
@@ -53,3 +63,7 @@ ran_for_regressor = RandomForestRegressor(n_estimators = 10,random_state = 0)
 ran_for_regressor.fit(features_train,labels_train)
 pred1 = ran_for_regressor.predict(features_test)
 score = ran_for_regressor.score(features_test,labels_test)    
+
+from sklearn.cross_validation import cross_val_score
+cv = cross_val_score(estimator = ran_for_regressor,X = features_train,y = labels_train,cv = 10)
+cv.mean()
